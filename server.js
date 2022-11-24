bf97a7d9226d82ffd6092e111194311bcf9d99a3
unknown <C:\Users\Admin\AppData\Roaming\The Bat!>
2022-11-24 09:04:36 -0500

    CDM11

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const express = require('express');
 const compression = require('compression');
 const connectDB = require('./config/dbConn')
 const mongoose = require('mongoose')
-const axios = require('axios')
 
 const app = express();
 const PORT = process.env.PORT || 3500
