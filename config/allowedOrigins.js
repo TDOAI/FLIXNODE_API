3ea29dbddab26bd0d46ccdb2beeee88fdfe14e6e
unknown <C:\Users\Admin\AppData\Roaming\The Bat!>
2022-12-01 17:53:28 -0500

    CDM-D-01-2

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,4 +1,10 @@
 const allowedOrigins = [
+    '192.168.1.2:3000',
+    '192.168.1.3:3000',
+    '192.168.1.4:3000',
+    '192.168.1.5:3000',
+    '192.168.1.6:3000',
+    '192.168.1.7:3000',
     'http://localhost:3000',
     'https://www.flixnode-test.vercel.app',
     'https://flixnode-test.vercel.app'
